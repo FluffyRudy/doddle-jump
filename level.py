9b9895e61ceb4fdb8ee9b6b26c33fdae80b030a6
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-07-25 22:44:15 +0545

    add score ui and score

--- level.py
+++ level.py
@@ -14,6 +14,9 @@ from constants import (
     JUMP_SPEED,
     SCROLLING_RECT,
     DEFAULT_PLATFORM_SIZE,
+    SCORE_CONTAINER_SIZE,
+    SCORE_CONTAINERE_BG,
+    BLACK,
 )
 
 
@@ -33,8 +36,14 @@ class Level:
         self.scrolling_rect = pygame.Rect(SCROLLING_RECT)
 
         self.create_platforms()
-
         self.player.hitbox.midbottom = self.platform_group.sprites()[0].rect.topleft
+
+        self.total_scroll = 0
+        self.score = 0
+        self.font = pygame.font.Font(None, 50)
+        self.score_surface = pygame.Surface(SCORE_CONTAINER_SIZE, pygame.SRCALPHA)
+        self.score_surface.fill(pygame.Color(SCORE_CONTAINERE_BG))
+        self.score_surface.set_alpha(100)
 
     def create_platforms(self):
         platforms_len = len(self.platform_group.sprites())
@@ -68,10 +77,18 @@ class Level:
             self.player.hitbox.y += scroll_amount
             for platform in self.platform_group.sprites():
                 platform.scroll(scroll_amount)
+            self.total_scroll += scroll_amount
 
-    def draw(self):
-        self.visible_group.draw(self.main_surface)
-        self.player_group.draw(self.main_surface)
+    def update_score(self):
+        self.score = int(self.total_scroll // 10)
+
+    def display_score(self):
+        score_ui = self.font.render(f"{self.score}", True, pygame.Color(BLACK))
+        score_x = (self.score_surface.get_width() - score_ui.get_width()) // 2
+        score_y = (self.score_surface.get_height() - score_ui.get_height()) // 2
+        self.score_surface.blit(score_ui, (score_x, score_y))
+        self.main_surface.blit(self.score_surface, (0, 0))
+        self.score_surface.fill(SCORE_CONTAINERE_BG)
 
     def update(self):
         self.handle_platform_collision()
@@ -79,7 +96,12 @@ class Level:
         self.player_group.update()
         self.create_platforms()
         self.scroll()
-        # pygame.draw.rect(self.main_surface, "red", self.scrolling_rect)
+        self.update_score()
+
+    def draw(self):
+        self.visible_group.draw(self.main_surface)
+        self.player_group.draw(self.main_surface)
+        self.display_score()
 
     def run(self):
         self.update()
