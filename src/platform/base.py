62cf2dbeba165c7d1d14b0d0e9d29c34c84a63b8
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-07-26 12:34:02 +0545

    Remove edge position correction from `Platform` class and new class method to track the previous platform position

--- src/platform/base.py
+++ src/platform/base.py
@@ -1,11 +1,14 @@
 from typing import Tuple
 from pygame.sprite import Sprite
 from pygame_utility.image_util import load_image
-from constants import WIDTH, HEIGHT, DEFAULT_PLATFORM_SIZE
+from constants import WIDTH, HEIGHT, DEFAULT_PLATFORM_SIZE, SCREEN_CENTER
 from config import GRAPHICS
 
 
 class Platform(Sprite):
+    last_x = (SCREEN_CENTER[0] - DEFAULT_PLATFORM_SIZE[0]) // 2
+    last_y = HEIGHT - DEFAULT_PLATFORM_SIZE[1]
+
     def __init__(
         self, position: Tuple[int, int], size: Tuple[int, int] = DEFAULT_PLATFORM_SIZE
     ):
@@ -13,14 +16,17 @@ class Platform(Sprite):
         self.image = load_image(GRAPHICS / "platform.png", scale_size=size)
         self.rect = self.image.get_rect(topleft=position)
 
-        if self.rect.right > WIDTH:
-            self.rect.right = WIDTH
-        elif self.rect.left < 0:
-            self.rect.left = 0
+    @classmethod
+    def update_position(cls, position: Tuple[int, int]):
+        cls.last_x = position[0]
+        cls.last_y = position[1]
+
+    @classmethod
+    def get_last_pos(cls) -> Tuple[int, int]:
+        return cls.last_x, cls.last_y
 
     def scroll(self, distance: int):
         self.rect.y += distance
 
-    def update(self, *args, **kwargs):
-        if self.rect.top > HEIGHT:
-            self.kill()
+    def isoffview(self):
+        return self.rect.top > HEIGHT
